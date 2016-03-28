77359ee32a077c4a5f8423fbe6d07b3595c3a7d8
hyajima <hyajima.1014@gmail.com>
2016-03-28 09:24:55 +0900

    add httpd and readme

--- cookbooks/httpd/recipes/default.rb
+++ cookbooks/httpd/recipes/default.rb
@@ -1,5 +1,25 @@
-package "httpd" do
-  action :install
+case node[:platform]
+ when 'redhat', 'centos'
+   package "httpd" do
+     action :install
+   not_if 'which httpd'
+ end
+ 
+ when 'debian', 'ubuntu'
+   package "apache2" do
+    action :install
+   not_if 'which apache'
+ end
 end
 
+directory '/usr/local/hogehoge/' do
+ owner 'vagrant'
+ group 'vagrant'
+ mode 0755
+ action :create
+end
 
+service "httpd" do
+ action [:enable, :start]
+ supports :status => true, :restart => true, :reload => true
+end
---
+++ cookbooks/perl/recipes/default.rb
@@ -0,0 +1,6 @@
+=begin
+package "perl" do
+  action :install
+  version "4.5.10"
+end
+=end
