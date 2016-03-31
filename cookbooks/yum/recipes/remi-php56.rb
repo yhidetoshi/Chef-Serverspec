0195126cbbf70998c0b44191d5baaa82197a3d71
hyajima <hyajima.1014@gmail.com>
2016-03-31 13:41:00 +0900

    add git_cookbook modified php56_cookbook

--- cookbooks/yum/recipes/remi-php56.rb
+++ cookbooks/yum/recipes/remi-php56.rb
@@ -1,6 +1,6 @@
 yum_repository 'remi-php56' do
   description 'for php 5.6'
-  mirrorlist 'http://rpms.remirepo.net/enterprise/6/php56/mirror'
-  gpgkey 'http://rpms.famillecollet.com/enterprise/remi.repo'
+  mirrorlist 'http://rpms.remirepo.net/enterprise/$releasever/php56/mirror'
+  gpgkey 'http://rpms.remirepo.net/RPM-GPG-KEY-remi'
   action :create
 end
