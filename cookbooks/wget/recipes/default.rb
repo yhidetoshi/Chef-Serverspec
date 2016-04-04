68dd3f9c7911e974373e29f48118ca6084b46ea6
yhidetoshi <hyajima.1014@gmail.com>
2016-04-04 11:26:00 +0900

    add jenkins_cookbook and use indlude_recipe

--- cookbooks/wget/recipes/default.rb
+++ cookbooks/wget/recipes/default.rb
@@ -1,8 +1,5 @@
-#
-# Cookbook Name:: wget
-# Recipe:: default
-#
-# Copyright 2016, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+package "wget" do
+  action :install
+  not_if "which wget"
+end
+
